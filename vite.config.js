d15edcd56be9198cf9ba636d520e76379df06520
Darwin <139725726+MeDarwin@users.noreply.github.com>
2024-01-28 20:08:56 +0700

    fix(attempt): fix build error, set base to "/"

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,9 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  if (command === "serve") {
+export default defineConfig(() => {
     return {
       base: "/",
-      plugins: [react()],
-    };
-  } else
-    return {
-      base: "/react-tutorial/",
       plugins: [react()],
     };
 });
