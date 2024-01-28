31652d2aa4734b87cea046072251fe66bbfe3b3d
Darwin <139725726+MeDarwin@users.noreply.github.com>
2024-01-28 18:11:54 +0700

    update(config): update dev and build differ

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/react-tutorial/',
-  plugins: [react()],
-})
+export default defineConfig(({ command }) => {
+  if (command === "serve") {
+    return {
+      base: "/",
+      plugins: [react()],
+    };
+  } else
+    return {
+      base: "/react-tutorial/",
+      plugins: [react()],
+    };
+});
