482b81495fba5a9124b1aaab756540ffc3b55193
Darwin <139725726+MeDarwin@users.noreply.github.com>
2024-01-28 20:33:15 +0700

    fix(route): fix route base for deploy

--- src/routes.jsx
+++ src/routes.jsx
@@ -5,29 +5,32 @@ import Pokemon from "./pages/Pokemon";
 import { pokemonApi } from "./services/pokemonApi";
 import { store } from "./store";
 
-export const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Index />,
-  },
-  {
-    path: "/pokemon",
-    element: <Pokemon />,
-    loader: async ({ request }) => {
-      const url = new URL(request.url);
-      const offset = url.searchParams.get("offset");
-      const req = store.dispatch(
-        pokemonApi.endpoints.getPokemons.initiate(offset)
-      );
-      const pokemon = req.unwrap();
-      req.unsubscribe();
-      return defer({
-        pokemon,
-      });
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Index />,
     },
-  },
-  {
-    path: "/jsonplaceholder",
-    element: <JsonPlaceholder />,
-  },
-]);
+    {
+      path: "/pokemon",
+      element: <Pokemon />,
+      loader: async ({ request }) => {
+        const url = new URL(request.url);
+        const offset = url.searchParams.get("offset");
+        const req = store.dispatch(
+          pokemonApi.endpoints.getPokemons.initiate(offset)
+        );
+        const pokemon = req.unwrap();
+        req.unsubscribe();
+        return defer({
+          pokemon,
+        });
+      },
+    },
+    {
+      path: "/jsonplaceholder",
+      element: <JsonPlaceholder />,
+    },
+  ],
+  { basename: import.meta.env.DEV ? "/" : "/react-tutorial/" }
+);
